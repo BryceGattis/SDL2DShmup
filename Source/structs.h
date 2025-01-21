f5bc097eb5ef6e85e800dca423963b758633a4da
BryceGattis <brycegattis@yahoo.com>
2025-01-21 08:12:12 -0600

    Add Entity struct

---
+++ Source/structs.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+struct Entity
+{
+    int x;
+    int y;
+    int dx;
+    int dy;
+    Entity(const int x, const int y, const int dx, const int dy) : x(x), y(y), dx(dx), dy(dy) {}
+};
